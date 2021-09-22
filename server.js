cc97f22d38eb187d915a2b092a784766be9105a6
magantus@googlemail.com <Extrem93!>
2021-09-23 00:37:59 +0200

    c

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + 'carstore'));
+app.use(express.static(__dirname + '/carstore'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + 'carstore/index.html'));
+    res.sendFile(path.join(__dirname + '../www/index.html'));
 
 });
 
