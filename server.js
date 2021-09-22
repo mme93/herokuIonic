df467030870f4c3a167dc9d29b8a31e7c9d6ee53
magantus@googlemail.com <Extrem93!>
2021-09-23 00:43:15 +0200

    c

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/carstore'));
+app.use(express.static(__dirname + '/www'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '../www/index.html'));
 
 });
 
-app.listen(process.env.PORT || 8100);
+
+app.set('port', process.env.PORT || 8100);
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
